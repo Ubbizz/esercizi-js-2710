ba4764fd32480501b827dc9eba3aa6cc662be9d7
Mattia <bubibubi996@proton.me>
2025-10-24 16:36:05 +0200

    esercizi-js-2710

---
+++ esercizi.js
@@ -0,0 +1,20 @@
+//Esercizio 6 rifatto con "while"
+
+// let number = 1;
+
+// while (number <= 100) {
+//     console.log(number++ ** 2);
+// }
+
+
+//Esercizio 7 rifatto con "while"
+
+// let number = 0;
+
+// while (number <= 99) {
+//     if (number++ % 2 === 0) {
+//         console.log("dispari  " + number);
+//     } else {
+//         console.log("pari " + number);  
+//     }
+// }
