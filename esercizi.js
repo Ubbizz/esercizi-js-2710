7ff4e68a8c8e99b865ff9b87b741c80865109dde
Mattia <bubibubi996@proton.me>
2025-10-24 20:09:32 +0200

    olè

--- esercizi.js
+++ esercizi.js
@@ -92,5 +92,77 @@
 // alert("Hai vissuto circa " + oreVissute + " ore!");    
 
 
+// 1 esercizio libro: Write a loop that makes seven calls to console.log to output the following triangle:
+
+
+// for (let a = 0; a <= 0; a++) {
+//     console.log("#");
+//     for (let b = 0; b < 1; b++) {
+//         console.log("##");
+//         for (let c = 0; c < 1; c++) {
+//             console.log("###");
+//             for (let d = 0; d < 1; d++) {
+//                 console.log("####");
+//                 for (let e = 0; e < 1; e++) {
+//                     console.log("#####");
+//                     for (let f = 0; f < 1; f++) {
+//                         console.log("######");
+//                         for (let g = 0; g < 1; g++) {
+//                             console.log("#######");
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Con CHATGPT più corto
+
+// for (let i = 1; i <= 7; i++) {
+//   console.log("#".repeat(i));
+// }
+
+
+// 2 esercizio libro: FIZZBUZZ - Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) { // && è l'operatore AND e lo puoi utilizzare nelle stringhe con più operazioni
+//     console.log("FizzBuzz"); 
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");     
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");     
+//   } else {
+//     console.log(i);          
+//   }
+// }
+
+
+// 3 esercizio libro: Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+// When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+
+
+// let size = parseInt(prompt("Dimmi un numero"));
+// let board = "";
+
+// for (let row = 0; row < size; row++) {
+//   for (let col = 0; col < size; col++) {
+//     // Alterna fra # e spazio
+//     if ((row + col) % 2 === 0) { 
+//       board += " ";
+//     } else {
+//       board += "#"; 
+//     }
+//   }
+//   board += "\n"; // Nuova colonna dopo ogni linea
+// } // Ritorna al codice nestato finchè "row" non è maggiore del "size" scritto nel prompt 
+   // finito il loop outputta sulla console
+// console.log(board);
+
+
+
+
 
 
